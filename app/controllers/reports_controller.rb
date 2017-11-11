d1d7fa26bc2febc808fa0cf4800e0657e106b6d9
SarahJS <sedky.sarah@gmail.com>
2017-11-10 21:32:21 -0800

    Added some more form things. Can't remember if this works or not but I'll push again soon.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,47 @@
 class ReportsController < ApplicationController
 	
-	# Create a new report
-	def new
+	def index
+		@reports = Report.all
 	end
+
+	# Show report
+	def show
+		@report = Report.find(params[:id])
+	end
+
+	# New report
+	def new
+		@report = Report.new
+	end
+
+	# edit an existing report
+	def edit
+		@report = Report.find(params[:id])
+	end
+
+	# Create the report
+	def create
+  		@report = Report.new(report_params)
+ 
+  		if @report.save
+  			redirect_to @report
+  		else
+  			render 'new'
+  		end
+	end
+
+	def update
+		@report = Report.find(params[:id])
+
+		if @report.update(report_params)
+			redirect_to @report
+		else
+			render 'edit'
+		end
+	end
+ 
+	private
+	  def report_params
+	    params.require(:report).permit(:title, :text)
+	  end
 end
